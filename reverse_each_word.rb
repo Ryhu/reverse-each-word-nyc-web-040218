feabfcf9b16c032e21cb6f87c9ca814a0e7e6862
Ryan Hu <ryanhuuu@gmail.com>
2018-03-21 00:21:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,8 +11,8 @@ end
 def reverse_each_word(str)
   arr = str.split(" ")
   result = ""
-  arr.each do |word|
-    result += word.reverse + " "
+  arr.collect do |word|
+    word.reverse + ""
   end
   
   return result[0..-2]
